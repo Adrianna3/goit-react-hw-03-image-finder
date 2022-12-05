168f376aeb3a3495ac3267c7cbd4b329baee4c0c
Adrianna Staniek <adrstani@wp.pl>
2022-12-05 19:27:25 +0100

    finished

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import styles from './Button.module.scss';
+import styles from './Button.module.css';
+
+export const Button = ({loadMore}) => {
+  const {btn} = styles;
+
+  return (
+    <>
+     <button 
+      type="button" 
+      className={btn} 
+      onClick={loadMore}
+    >
+      Load more
+    </button> 
+    </>
+  )
+}
+
+Button.propTypes = {
+  loadMore: PropTypes.func.isRequired,
+}
+
+export default Button;
